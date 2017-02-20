b530a1b4ab9a44d3426ddc9e1e2e6d5bc2b87f91
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-02-20 18:36:32 +0100

    Cleaning

--- ReactSkeleton/src/main.jsx
+++ ReactSkeleton/src/main.jsx
@@ -1,5 +1,4 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Routes = require('./Routes.jsx');
 
-ReactDOM.render(Routes, document.getElementById('main'));
+ReactDOM.render(/**/, document.getElementById('main'));
