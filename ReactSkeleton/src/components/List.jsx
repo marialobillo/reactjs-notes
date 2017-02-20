4c83feecf9f821a14b59df31afdb1218a68535c8
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-02-20 19:40:37 +0100

    An example

---
+++ ReactSkeleton/src/components/List.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var List =React.createClass({
+  render: function(){
+      return (
+        <div>Hola</div>
+      );
+    }
+  });
+
+module.exports = List;
--- ReactSkeleton/src/main.jsx
+++ ReactSkeleton/src/main.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var List = require('./components/List.jsx');
 
-ReactDOM.render(/**/, document.getElementById('main'));
+ReactDOM.render(<List/>, document.getElementById('main'));
