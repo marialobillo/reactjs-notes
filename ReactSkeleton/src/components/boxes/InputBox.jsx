2b61cd5e9479d164eddae650e79be6a38ea08337
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-02-20 19:43:59 +0100

    Boxes

---
+++ ReactSkeleton/src/components/boxes/InputBox.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var InputBox = React.createClass({
+  render: function(){
+    return (
+      <div className="row">
+
+      </div>
+    );
+  }
+});
+
+module.exports = InputBox;
---
+++ ReactSkeleton/src/components/boxes/ResultBox.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var ResultBox = React.createClass({
+  render: function(){
+    return (
+      <div className="row">
+
+      </div>
+    );
+  }
+});
+
+module.exports = ResultBox;
