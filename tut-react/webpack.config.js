e120162631c9719ae8cda2be2ffa3c6a90566c44
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-02-22 19:42:13 +0100

    Loaders

--- tut-react/webpack.config.js
+++ tut-react/webpack.config.js
@@ -5,6 +5,19 @@ module.exports = {
   context: __dirname,
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./app/dist/index.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components),
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attr', 'transform-class-properties', 'transform-decorators-legacy']
+        }
+      }
+    ]
+  },
   output: {
     path: __dirname + "/app/js",
     filename: "index.min.js"
