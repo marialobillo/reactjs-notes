78f8dc13ba7f5b0ecfa31eef96a2cd25ef158e42
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-02-24 19:48:22 +0100

    browserify

--- 9.shop/gulpfile.js
+++ 9.shop/gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp');
 var LiveServer = require('gulp-live-server');
 var browserSync =require('browser-sync');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
 
 gulp.task('live-server', function(){
   var server = new LiveServer('server/main.js');
   server.start();
+})
+
+gulp.task('bundle', function(){
+  return browserify({
+    entries: 'app/main.jsx',
+    debug: true,
+  })
+  .transform(babelify)
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./.tmp'));
 })
 
 gulp.task('server', ['live-server'], function(){
