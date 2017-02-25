2fabbdb1bc70ca099f63d1808056461fab2bbf00
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-02-25 13:00:10 +0100

    Creating a component

--- 9.shop/app/main.jsx
+++ 9.shop/app/main.jsx
@@ -1,1 +1,5 @@
+var GroceryItemList = require('./components/GroceryItemList.jsx');
+var React = require('react/addons');
+
+ReactDOM.render(<GroceryItemList />, app);
 console.log("Hello from JSX!");
