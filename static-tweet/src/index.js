e085078987ffe0fac9c53d166fd15c621e632f54
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-02-26 12:43:02 +0100

    Adding an avatar from a component

--- static-tweet/src/index.js
+++ static-tweet/src/index.js
@@ -2,14 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+var Avatar = React.createClass({
+  render: function(){
+    return (
+      <img src="http://www.gravatar.com/avatar/nothing"
+        className="avatar"
+        alt="avatar" />
+    );
+  }
+});
+
 var Tweet = React.createClass({
   render: function(){
     return (
       <div className="tweet">
+        <Avatar/>
         Tweet
       </div>
     );
   }
 });
+
+
 
 ReactDOM.render(<Tweet/>, document.querySelector('#root'));
