dffe826ef7a6113383e4e9cd67d73150b066390c
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-02-26 12:22:30 +0100

    Tweet component

--- static-tweet/src/index.js
+++ static-tweet/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+var Tweet = React.createClass({
+  render: function(){
+    return (
+      <div className="tweet">
+        Tweet
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<Tweet/>, document.querySelector('#root'));
