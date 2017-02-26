729f22f877ac24ba072481033adc3627ed6e9e32
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-02-26 18:10:59 +0100

    Refactoring

--- react-hello/src/index.js
+++ react-hello/src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var Hello = React.createClass({
+  render: function(){
+    return (
+      <span>Hello</span>
+    );
+  }
+});
+
+var World = React.createClass({
+  render: function(){
+    return (
+      <span>World</span>
+    );
+  }
+});
+
 var HelloWorld = React.createClass({
   render: function(){
     return (
-      <div>Hello World! xoxoxoxo</div>
+      <div>
+        <Hello/> <World/>!
+      </div>
     );
   }
 });
