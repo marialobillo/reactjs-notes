f7739d2bc3d4412be7c10e8c244da90fcc91e96f
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-02-27 20:01:39 +0100

    Avatar, time, message..etc

--- props-tweet/src/index.js
+++ props-tweet/src/index.js
@@ -47,7 +47,7 @@ var Message = React.createClass({
   render: function(){
     return(
       <div className="message">
-        This is less than 140 characters.
+        {this.props.text}
       </div>
     );
   }
@@ -66,8 +66,12 @@ var NameWithHandle =React.createClass({
 
 var Avatar = React.createClass({
   render: function(){
+    var hash = this.props.hash;
+    var url = `https://s.gravatar.com/avatar/${hash}`;
+
+    //https://s.gravatar.com/avatar/5ab1672865368e1c44631db6941719c0?s=80
     return (
-      <img src="http://www.gravatar.com/avatar/nothing"
+      <img src={url}
         className="avatar"
         alt="avatar" />
     );
@@ -78,10 +82,11 @@ var Tweet = React.createClass({
   render: function(){
     return (
       <div className="tweet">
-        <Avatar/>
+        <Avatar hash={this.props.tweet.gravatar}/>
         <div className="content">
-          <NameWithHandle/><Time/>
-          <Message/>
+          <NameWithHandle author={this.props.tweet.author}/>
+          <Time time={this.props.tweet.timestamp}/>
+          <Message text={this.props.tweet.message}/>
           <div className="buttons">
             <ReplyButton/>
             <RetweetButton/>
@@ -94,6 +99,16 @@ var Tweet = React.createClass({
   }
 });
 
+var testTweet = {
+  message: "Something about cats.",
+  gravatar: "5ab1672865368e1c44631db6941719c0?s=80",
+  author: {
+    handle: "catperson",
+    name: "IAMA Cat Person"
+  },
+  likes: 2,
+  retweets: 0,
+  timestamp: "2016-07-30 21:24:37"
+};
 
-
-ReactDOM.render(<Tweet/>, document.querySelector('#root'));
+ReactDOM.render(<Tweet tweet={testTweet}/>, document.querySelector('#root'));
