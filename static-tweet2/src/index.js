6c0ee16ad67e8ca8b9edd8dc43d5c5ce413459ab
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-03-03 12:39:30 +0100

    Test commits

--- static-tweet2/src/index.js
+++ static-tweet2/src/index.js
@@ -21,7 +21,37 @@ let FileList = React.createClass({
       </table>
     );
   }
-});
+})
+
+conts testFiles = [
+  {
+    id: 1,
+    name: 'src',
+    type: 'folder',
+    updated_at: "2016-07-11 21:24:00",
+    latestCommit: {
+      message: 'Initial Commit'
+    }
+  },
+  {
+    id: 2,
+    name: 'tests',
+    type: 'folder',
+    updated_at: "2016-07-11 21:24:00",
+    latestCommit: {
+      message: 'Initial Commit'
+    }
+  },
+  {
+    id: 3,
+    name: 'README',
+    type: 'file',
+    updated_at: "2016-07-11 21:24:00",
+    latestCommit: {
+      message: 'Added a readme'
+    }
+  },
+];
 
 
 ReactDOM.render(
