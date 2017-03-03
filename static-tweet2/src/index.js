981a038efabe83be7faea46904cc092ea6b1b45b
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-03-03 12:33:45 +0100

    FileList Component

--- static-tweet2/src/index.js
+++ static-tweet2/src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+let FileList = React.createClass({
+  propTypes: {
+    files: React.PropTypes.array
+  },
+  render(){
+    let files = this.props.files;
+    return (
+      <table className="file-list">
+        <tbody>
+          {files.map(file => (
+            <tr className="file-list-item" key={file.id}>
+              <td className="file-name">{file.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+});
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
