30b65188a94777f34ab355c7e4436e83a08a45d1
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-03-06 12:50:58 +0100

    News on a list

---
+++ hack-news/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+var News = React.createClass({
+  render(){
+    return (
+      <div>
+        News
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  < />,
+  document.getElementById('root')
+);
