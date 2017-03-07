5c2a2320ea61091224b7d5de64e32c09f8202fdb
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-03-07 19:10:20 +0100

    The menu

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -2,19 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedTab: 0
+    };
+  }
+
+  selectTab = (index) => {
+    this.setState({selectedTab: index});
+  }
   renderContent(){
-    return (
-      <span>Empty</span>
-    );
+    switch(this.state.selectedTab){
+      default:
+        case 0: return <span>Items</span>;
+        case 1: return <span>Cart</span>;
+    }
   }
 
   render() {
+    let {selectedTab} = this.state;
+
     return (
       <div className="App">
         <nav className="App-nav">
           <ul>
-            <li className="App-nav-item"><a>Items</a></li>
-            <li className="App-nav-item"><a>Cart</a></li>
+            <li className={`App-nav-item ${selectedTab === 0 && 'selected'}`}>
+              <a onClick={this.selectTab.bind(this, 0)}>Items</a>
+            </li>
+            <li className={`App-nav-item ${selectedTab === 1 && 'selected'}`}>
+              <a onClick={this.selectTab.bind(this, 1)}>Cart</a>
+            </li>
           </ul>
         </nav>
         <main className="App-content">
