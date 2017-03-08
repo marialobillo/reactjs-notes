e90755825eaeca6df2b5d72b1d27c2ae1ef66d0f
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-03-08 10:34:22 +0100

    Export item page

--- shopper/src/ItemPage.js
+++ shopper/src/ItemPage.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './ItemPage.css';
+
+function ItemPage({item}){
+  return (
+    <ul className="ItemPage-items">
+      {items.map(item =>
+        <li key={item.id} className="Itempage-item">
+          {item.name}
+        </li>
+      )}
+    </ul>
+  );
+}
+
+ItemPage.propTypes = {
+  items: React.PropTypes.array.isRequired
+};
+
+export default ItemPage;
