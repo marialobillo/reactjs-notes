9865a5ce4784fae587e76646b559368174d93e09
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-03-15 13:16:24 +0100

    Cleaning

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
-
 const API_KEY = 'AIzaSyBq2Kci0AzeFs63SdZNKdU58Xeco6zJzE4';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
 // Create a new component. This component should produce some HTML
-const App = () => {
-  return (<div>
-    <SearchBar />
-  </div>
-);
+class App extends Component {
+  constructor(){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      //
+    });
+  }
+
+  render(){
+      return (<div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
