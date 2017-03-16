616a72e381b2de7432e5c0bceafdb53875497bde
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-03-16 12:33:14 +0100

    Props props props

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyBq2Kci0AzeFs63SdZNKdU58Xeco6zJzE4';
 
 // Create a new component. This component should produce some HTML
 class App extends Component {
-  constructor(){
+  constructor(props){
     super(props);
 
     this.state = { videos: [] };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       this.setState({ videos });
-      //
     });
   }
 
