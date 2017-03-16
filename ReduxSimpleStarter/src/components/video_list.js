cf4b8122f21c171c89436ce71234c26a7183b073
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-03-16 12:40:38 +0100

    Key

--- ReduxSimpleStarter/src/components/video_list.js
+++ ReduxSimpleStarter/src/components/video_list.js
@@ -5,6 +5,7 @@ const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
     return (
       <VideoListItem
+        key={video.etag}
         video={video} />
     );
   });
