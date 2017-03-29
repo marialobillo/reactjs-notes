4b1791d7c5bc9b4db705be3ac7df6d199e12604f
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-03-29 10:19:31 +0200

    Removing unnecessary variables

--- jasmine-example/spec/AddressBookSpec.js
+++ jasmine-example/spec/AddressBookSpec.js
@@ -1,18 +1,24 @@
 describe('Adress Book', function(){
-  it('should be able to add a contact', function(){
-    var addressBook = new AddressBook(),
-        thisContact = new Contact();
+  var addressBook,
+      thisContact;
 
+  beforeEach(function(){
+    addressBook = new AddressBook();
+    thisContact = new Contact();
+  });
+
+
+  it('should be able to add a contact', function(){
     addressBook.addContact(thisContact);
 
     expect(addressBook.getContact(0)).toBe(thisContact);
   });
 
-  if('should be able to delete a contact', function(){
-    var addressBook = new AddressBook(),
-        thisContact = new Contact();
+  it('should be able to delete a contact', function(){
 
     addressBook.addContact(thisContact);
     addressBook.deleteContact(0);
+
+    expect(addressBook.getContact(0)).not.toBeDefined();
   });
 });
