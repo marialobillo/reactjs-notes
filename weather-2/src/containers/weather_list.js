96daa74efe61e8c520b8d7591cb0b29c8be02f1a
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-03 10:27:28 +0200

    Removing errors

--- weather-2/src/containers/weather_list.js
+++ weather-2/src/containers/weather_list.js
@@ -9,11 +9,11 @@ class WeatherList extends Component{
     const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
-    const {lon, lat} = cityData.city.coord.lon;
+    const {lon, lat} = cityData.city.coord;
 
     return(
       <tr key={name}>
-        <td><GoogleMap /></td>
+        <td><GoogleMap lon={lon} lat={lat}/></td>
         <td><Chart data={temps} color="orange" units="ºC"/></td>
         <td><Chart data={pressures} color="blue" units="hPa" /></td>
         <td><Chart data={humidities} color="green" units="%" /></td>
