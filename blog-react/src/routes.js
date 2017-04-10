dd5792d6070bc7b844a8757b220829cba86ab94c
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-10 17:30:20 +0200

    Adding redux-promise

--- blog-react/src/routes.js
+++ blog-react/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
+import PostsIndex from './components/posts_index';
 
 const Greeting = () => {
   return <div>Hey there!</div>;
@@ -8,8 +9,6 @@ const Greeting = () => {
 
 export default(
 <Route path="/" component={App} >
-  <Route path="/greet" component={Greeting} />
-  <Route path="/greet2" component={Greeting} />
-  <Route path="/greet3" component={Greeting} />
+  <IndexRoute component={PostsIndex}/>
 </Route>
 );
