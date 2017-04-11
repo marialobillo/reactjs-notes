1cbb220b44b461826078d0b107bddb6b27cd39eb
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-11 20:30:09 +0200

    fetch_posts

--- blog-react/src/reducers/reducer_posts.js
+++ blog-react/src/reducers/reducer_posts.js
@@ -1,7 +1,11 @@
-const INITIAL_STATE = {};
+const INITIAL_STATE = { all: [], post: null};
+
+import { FETCH_POSTS } from '../actions/index';
 
 export default function(state = INITIAL_STATE, action){
   switch(action.type){
+    case FETCH_POSTS:
+      return { ...state, all: action.payload.data };
     default:
       return state;
   }
