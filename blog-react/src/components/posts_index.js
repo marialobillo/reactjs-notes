ef07a9d0c434ae3b2f69ee28f32d355a295dbf5f
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-12 20:11:24 +0200

    Connecting

--- blog-react/src/components/posts_index.js
+++ blog-react/src/components/posts_index.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionsCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return <div>List of Blog posts.</div>;
-};
+class PostsIndex extends Component{
+  componentsWillMount(){
+    console.log('This would be a good time to call an action creator to fetch posts.');
+  }
+
+  render(){
+    return (
+      <div>List of Blog Posts</div>
+    );
+  }
+}
+
+export default PostsIndex;
--- blog-react/src/reducers/reducer_posts.js
+++ blog-react/src/reducers/reducer_posts.js
@@ -2,6 +2,7 @@ const INITIAL_STATE = { all: [], post: null};
 
 import { FETCH_POSTS } from '../actions/index';
 
+
 export default function(state = INITIAL_STATE, action){
   switch(action.type){
     case FETCH_POSTS:
