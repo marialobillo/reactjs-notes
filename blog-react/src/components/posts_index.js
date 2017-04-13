f89c7cbf5a260e9a89e2f8e53b3dec8ab0257f2a
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-13 17:54:07 +0200

    Fixing

--- blog-react/src/components/posts_index.js
+++ blog-react/src/components/posts_index.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionsCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
-class PostsIndex extends Component{
-  componentsWillMount(){
-    console.log('This would be a good time to call an action creator to fetch posts.');
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
   }
 
-  render(){
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <Link to={"posts/" + post.id}>
+            <span className="pull-xs-right">{post.categories}</span>
+            <strong>{post.title}</strong>
+          </Link>
+        </li>
+      );
+    });
+  }
+
+  render() {
     return (
-      <div>List of Blog Posts</div>
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+      </div>
     );
   }
 }
 
-function mapDispatchToProps(dispatch){
-  return bindActionsCreators({fetchPosts}, dispatch);
+function mapStateToProps(state) {
+  return { posts: state.posts.all };
 }
 
-export default connect(null, mapDispatchToProps)(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
--- blog-react/src/index.js
+++ blog-react/src/index.js
@@ -7,8 +7,9 @@ import reducers from './reducers';
 import routes from './routes';
 import promise from 'redux-promise';
 
-
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(
+  promise
+)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
