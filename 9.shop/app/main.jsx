458a48eb903aa616c554d58bedcdf1fe63ef9d94
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-14 17:11:04 +0200

    Removing

--- 9.shop/app/main.jsx
+++
@@ -1,5 +0,0 @@
-var GroceryItemList = require('./components/GroceryItemList.jsx');
-var React = require('react/addons');
-
-ReactDOM.render(<GroceryItemList />, app);
-console.log("Hello from JSX!");
