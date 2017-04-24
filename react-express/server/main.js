f7d898a5a05e8b9ed95aa2acfc995c045c7b9d60
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-24 09:55:36 +0200

    Express

---
+++ react-express/server/main.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.render('./../app/index.ejs', {})
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
