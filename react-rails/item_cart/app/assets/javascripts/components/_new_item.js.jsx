c391f08d3aaffbd8889f54f6790fb1669fb4697c
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-28 10:08:21 +0200

    Inputs and button

--- react-rails/item_cart/app/assets/javascripts/components/_new_item.js.jsx
+++ react-rails/item_cart/app/assets/javascripts/components/_new_item.js.jsx
@@ -1,8 +1,16 @@
 var NewItem = React.createClass({
+  handleClick(){
+    var name = this.refs.name.value;
+    var description = this.refs.description.value;
+
+    console.log('The name value is ' + name + 'the description value is' + description);
+  },
   render(){
     return (
       <div>
-        <h1>New Item</h1>
+        <input ref='name' placeholder='Enter the name of the item'/>
+        <input ref='description' placeholder='Enter a description' />
+        <button onClick={this.handleClick}>Submit</button>
       </div>
     )
   }
