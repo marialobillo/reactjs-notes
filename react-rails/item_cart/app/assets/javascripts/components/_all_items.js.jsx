229ff5df9cbdb97e259831fa6b4cfff2cbb21188
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-28 11:41:33 +0200

    Item component

--- react-rails/item_cart/app/assets/javascripts/components/_all_items.js.jsx
+++ react-rails/item_cart/app/assets/javascripts/components/_all_items.js.jsx
@@ -2,13 +2,16 @@ var AllItems = React.createClass({
   handleDelete(id){
     this.props.handleDelete(id);
   },
+  handleEdit(){
+
+  },
   render(){
     var items = this.props.items.map((item) => {
       return (
         <div key={item.id}>
-          <h3>{item.name}</h3>
-          <p>{item.description}</p>
-          <button onClick={this.handleDelete.bind(this, item.id)}>Delete</button>
+          <Item item={item}
+                handleDelete={this.handleDelete.bind(this.item.id)}
+                handleEdit={this.handleEdit} />
         </div>
       )
     });
