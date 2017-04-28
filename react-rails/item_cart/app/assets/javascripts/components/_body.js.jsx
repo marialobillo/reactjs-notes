9fbfe5cbdbb404ad93cf45abd299a0f2d8ebb4a7
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-28 11:10:23 +0200

    Deleting an item

--- react-rails/item_cart/app/assets/javascripts/components/_body.js.jsx
+++ react-rails/item_cart/app/assets/javascripts/components/_body.js.jsx
@@ -2,18 +2,35 @@ var Body = React.createClass({
   getInitialState(){
     return { items: []}
   },
-  handleSubmit(item){
-    console.log(item);
+  handleDelete(id){
+    //console.log('in handle delete');
+    $.ajax({
+      url: `/api/v1/items/${id}`,
+      type: 'DELETE',
+      success: () => {
+        this.removeItemClient(id);
+      }
+    });
+  },
+  removeItemClient(id){
+    var newItems = this.state.items.filter((item) => {
+      return item.id != id;
+    });
+    this.setState({ items: newItems });
   },
   componentDidMount(){
     console.log('Component mounted.');
     $.getJSON('/api/v1/items.json', (response) => { this.setState({items: response}) });
   },
+  handleSubmit(item){
+    var newState = this.state.items.concat(item);
+    this.setState({ items: newState })
+  },
   render(){
     return (
       <div>
         <NewItem handleSubmit={this.handleSubmit} />
-        <AllItems items={this.state.items} />
+        <AllItems items={this.state.items} handleDelete={this.handleDelete}/>
       </div>
     )
   }
--- react-rails/item_cart/app/assets/javascripts/components/_new_item.js.jsx
+++ react-rails/item_cart/app/assets/javascripts/components/_new_item.js.jsx
@@ -6,12 +6,10 @@ var NewItem = React.createClass({
       url: 'api/v1/items',
       type: 'POST',
       data: { item: {name: name, description: description} },
-      success: (response) => {
-        console.log('it worked!', response);
+      success: (item) => {
         this.props.handleSubmit(item);
       }
     });
-    console.log('The name value is ' + name + 'the description value is' + description);
   },
   render(){
     return (
