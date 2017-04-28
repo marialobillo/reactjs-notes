8c4a5e53be5fed874b9a427e332870742495eee8
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-28 10:29:14 +0200

    Props and states

--- react-rails/item_cart/app/assets/javascripts/components/_body.js.jsx
+++ react-rails/item_cart/app/assets/javascripts/components/_body.js.jsx
@@ -1,9 +1,19 @@
 var Body = React.createClass({
+  getInitialState(){
+    return { items: []}
+  },
+  handleSubmit(item){
+    console.log(item);
+  },
+  componentDidMount(){
+    console.log('Component mounted.');
+    $.getJSON('/api/v1/items.json', (response) => { this.setState({items: response}) });
+  },
   render(){
     return (
       <div>
-        <NewItem />
-        <AllItems />
+        <NewItem handleSubmit={this.handleSubmit} />
+        <AllItems items={this.state.items} />
       </div>
     )
   }
